9f1aed1a23f3038806d35ed649ca4e3e11396607
giovannimanetti11 <giovanni@perseodesign.com>
2022-03-12 16:20:15 +0100

    Added function to keep track of funders

--- contracts/CoinsinergyContract.sol
+++ contracts/CoinsinergyContract.sol
@@ -2,7 +2,13 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Coinsinergy {
+
+    address[] public funders;
     
     receive() external payable {}
-    function addFunds() external payable {}
+    function addFunds() external payable {
+        funders.push(msg.sender);
+    }
 }
+
+// const instance = await Coinsinergy.deployed()
