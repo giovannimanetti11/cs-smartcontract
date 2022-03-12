81bea219420dd9c9a4e3d054825eb62a71a8f2e4
giovannimanetti11 <giovanni@perseodesign.com>
2022-03-12 16:33:10 +0100

    Get all Funders

--- contracts/CoinsinergyContract.sol
+++ contracts/CoinsinergyContract.sol
@@ -6,9 +6,15 @@ contract Coinsinergy {
     address[] public funders;
     
     receive() external payable {}
+    
     function addFunds() external payable {
         funders.push(msg.sender);
+    }
+
+    function getAllFunders() external view returns (address[] memory) {
+        return funders;
     }
 }
 
 // const instance = await Coinsinergy.deployed()
+// instance.addFunds({value:"5500000000000000000", from: accounts[2]})
